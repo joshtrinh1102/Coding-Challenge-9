fd1e717b5ad19342d14dc3e1d3987707e3ac6ebc
joshtrinh1102 <nltrinh1102@gmail.com>
2024-06-13 23:03:52 -0400

    Implement interactive behaviors with D3.js.

--- example.js
+++ example.js
@@ -1,3 +1,8 @@
-function main(){
-    
-}
+
+function initializepage(){
+    d3.select("body").append("div")
+        .text("New Div");//create div elements as dataponts.
+
+    d3.selectall("div").addEventListener("mouseover",style())
+    d3.selectall("div").addEventListener("mouseout",style())// Add event listeners./
+};
